cc5b2c81f11477a743dded8c6c141faa4c7607db
LijsTh <lijsth@gmail.com>
2024-06-15 11:01:04 -0300

    Addedd logs + logic
    -  created unespecificed_protocol method to avoid code repetition
    - Added _handle_PacketIn so we can log packet drop information.
    
    Co-authored-by: Tomas Fabrizio Orsi <torsi@fi.uba.ar>

--- src/firewall.py
+++ src/firewall.py
@@ -76,7 +76,6 @@ class Firewall (EventMixin):
         match = of.ofp_match()
         match.dl_type = pkt.ethernet.IP_TYPE
 
-        print("policy: ", policy)
         if "banned_tuples" in policy:
             print(policy["banned_tuples"])
             a = IPAddr(policy["banned_tuples"][0])
@@ -109,11 +108,12 @@ class Firewall (EventMixin):
                 # Puse protocolo != UDP/TCP
                 # None -> Baneas con TCP y UDP
                 if match.nw_proto == None:
-                    matchPair = match.clone()
-                    matchPair.nw_proto = protToNumber["UDP"]
-                    matchPair.tp_src = int(policy["src_port"])
-                    matches.append(matchPair)
-                    match.nw_proto = protToNumber["TCP"]
+                    # matchPair = match.clone()
+                    # matchPair.nw_proto = protToNumber["UDP"]
+                    # matchPair.tp_src = int(policy["src_port"])
+                    # matches.append(matchPair)
+                    # match.nw_proto = protToNumber["TCP"]
+                    match = self.adunespecified_protocol(matches, policy, match, is_src = True)
                 
 
                 elif match.nw_proto != protToNumber["TCP"] and match.nw_proto != protToNumber["UDP"]:
@@ -123,12 +123,28 @@ class Firewall (EventMixin):
                 match.tp_src = int(policy["src_port"])
 
             if "dst_port" in policy:
+                if match.nw_proto == None:
+                    match = self.unespecified_protocol(matches, policy, match, is_src = False)
+                
+                elif match.nw_proto != protToNumber["TCP"] and match.nw_proto != protToNumber["UDP"]:
+                    raise Exception("Cannot ban TCP/UDP port whilst using non TCP/UDP protocol.")
                 match.tp_dst = int(policy["dst_port"])
+                
 
 
         matches.append(match)
         return matches
-    
+        
+    def unespecified_protocol(self, matches, policy, match, is_src = True):
+        matchPair = match.clone()
+        matchPair.nw_proto = protToNumber["UDP"]
+        if is_src:
+            matchPair.tp_src = int(policy["src_port"])
+        else:
+            matchPair.tp_dst = int(policy["dst_port"])
+        matches.append(matchPair)
+        match.nw_proto = protToNumber["TCP"]
+        return match
         
 
     def _handle_ConnectionUp (self, event):
@@ -146,6 +162,23 @@ class Firewall (EventMixin):
         print("Total rules installed: ", len(self.policies))
 
 
+    def _handle_PacketIn(self, event):
+        packet = event.parsed
+        eth = packet
+        
+        if eth.type == eth.IP_TYPE:
+            ip = packet.next
+            log.info("IPv4 packet: %s -> %s, protocol: %s", ip.srcip, ip.dstip, ip.protocol)
+            
+            if ip.protocol == ip.TCP_PROTOCOL:
+                tcp = ip.next
+                log.info("TCP packet: %s:%s -> %s:%s", ip.srcip, tcp.srcport, ip.dstip, tcp.dstport)
+            elif ip.protocol == ip.UDP_PROTOCOL:
+                udp = ip.next
+                log.info("UDP packet: %s:%s -> %s:%s", ip.srcip, udp.srcport, ip.dstip, udp.dstport)
+        elif eth.type == eth.ARP_TYPE:
+            arp = packet.next
+            log.info("ARP packet: %s -> %s, opcode: %s", arp.hwsrc, arp.hwdst, arp.opcode)
 
 def launch ():
     '''
